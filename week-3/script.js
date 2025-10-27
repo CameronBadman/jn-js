bb52f9e62921b11e60f2e55498daaabbee9b7b78
Cameron Badman <cbadwork@gmail.com>
2025-10-27 14:00:39 +1000

    adding week-3

---
+++ week-3/script.js
@@ -0,0 +1,70 @@
+// ============ ARRAYS []
+console.log('=== ARRAYS ===');
+
+const numbers = [1, 2, 3, 4, 5];
+console.log('Array:', numbers);
+console.log('First element:', numbers[0]);
+// TODO: Log the last element using array.length
+
+
+// ============ OBJECTS {}
+console.log('\n=== OBJECTS ===');
+
+// TODO: Create an object with { key: value, key: value }
+
+
+// TODO: Log the object and access properties with dot notation
+
+
+// ============ MAP
+console.log('\n=== MAP ===');
+
+// TODO: Use array.map(item => ...)
+// const doubled = ...
+
+
+// TODO: Use array.map(item => ...)
+// const squared = ...
+
+
+// ============ FILTER
+console.log('\n=== FILTER ===');
+
+// TODO: Use array.filter(item => condition)
+// const evens = ...
+
+
+// TODO: Use array.filter(item => condition)
+// const greaterThanTwo = ...
+
+
+// ============ ARRAY OF OBJECTS
+console.log('\n=== ARRAY OF OBJECTS ===');
+
+// TODO: Create array with objects inside [ { }, { } ]
+// const students = [...]
+
+
+// TODO: Use array.filter() to check a property
+// const highGrades = ...
+
+
+// TODO: Use array.map() to extract one property
+// const names = ...
+
+
+// ============ FINAL BOSS
+console.log('\n=== FINAL BOSS ===');
+
+const characters = [
+  { name: 'Peter Griffin', show: 'Family Guy', age: 42 },
+  { name: 'Lois Griffin', show: 'Family Guy', age: 40 },
+  { name: 'Peter Griffin', show: 'American Dad', age: 55 },
+  { name: 'Stewie Griffin', show: 'Family Guy', age: 1 },
+  { name: 'Glenn Quagmire', show: 'Family Guy', age: 61 }
+];
+
+// TODO: Chain filter() and map(), then get first element with [0]
+// const peterGriffin = ...
+
+// TODO: Log the result
